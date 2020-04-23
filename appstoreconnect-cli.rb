a465653f2156ba7079a79b149a4078dfaea96f8e
Oliver Jones <oliver@deeperdesign.com>
2020-04-23 21:35:12 +1000

    Remove the tag and update the branch

--- appstoreconnect-cli.rb
+++ appstoreconnect-cli.rb
@@ -2,8 +2,7 @@ class AppstoreconnectCli < Formula
   desc "Command-line tool for interacting with Apple Appstore Connect API"
   homepage "https://github.com/ittybittyapps/appstoreconnect-cli"
   head "https://github.com/ittybittyapps/appstoreconnect-cli.git",
-       :branch => "test-brew-formula",
-       :tag => "V1.2"
+       :branch => "master"
 
   depends_on :xcode => ["11.4", :build]
 
