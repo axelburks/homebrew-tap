40af0802f145b4b8bcfe1b7273074606e0f66d8a
Axel <11376445+axelburks@users.noreply.github.com>
2023-08-08 13:12:06 +0800

    Update appstoreconnect-cli.rb

--- appstoreconnect-cli.rb
+++ appstoreconnect-cli.rb
@@ -1,7 +1,7 @@
 class AppstoreconnectCli < Formula
   desc "Command-line tool for interacting with Apple Appstore Connect API"
-  homepage "https://github.com/ittybittyapps/appstoreconnect-cli"
-  head "https://github.com/ittybittyapps/appstoreconnect-cli.git",
+  homepage "https://github.com/axelburks/appstoreconnect-cli"
+  head "https://github.com/axelburks/appstoreconnect-cli.git",
        :branch => "master"
 
   depends_on :xcode => ["11.4", :build]
